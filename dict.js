5487a178321214f5958c5b2e5c504164c074bde6
declanrjb <declanrjb@gmail.com>
2023-08-10 21:44:04 -0400

    Initial commit

---
+++ dict.js
@@ -0,0 +1,7 @@
+/* annotations dictionary */
+var annotations_dict = {
+    'Vivamus magna libero': "hello world",
+    'per conubia nostra': "Sed hendrerit quis turpis sed congue. Praesent massa ante, rutrum quis arcu et, aliquam gravida velit. Suspendisse diam ante, rhoncus sed tristique nec, tempor et tellus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam sit amet turpis a ligula commodo hendrerit. Morbi elementum sed neque eu tincidunt. Aenean iaculis suscipit elit et dictum. Pellentesque pulvinar maximus nunc, at tempor turpis ullamcorper in. Etiam urna purus, bibendum at tincidunt et, dapibus eget leo.",
+    'Sed malesuada sed ipsum nec eleifend.': "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+    'lectus dapibus faucibus': "'There is no one who loves pain itself, who seeks after it and wants to have it, simply because it is pain...'"
+};
---
+++ script.js
@@ -0,0 +1,55 @@
+/* actual script */
+var classname = ".note";
+
+var annot_name = ".annotation";
+
+var elms=document.querySelectorAll(classname);
+
+var annotations = document.querySelectorAll(annot_name);
+var annotation = annotations[0];
+
+var annot_paragraph = annotation.querySelector("p");
+
+for(var i=0;i<elms.length;i++){
+    elms[i].onclick = function(){
+
+        annotation.style.transitionDuration = "1s";
+
+        var new_annotation = annotations_dict[this.textContent];
+
+        annot_paragraph.textContent = new_annotation;
+
+        var annotation_position = annotation.getBoundingClientRect();
+
+        var mid_height = annotation_position.height / 2;
+
+        var self_position = this.getBoundingClientRect();
+        var self_y_formatted = self_position.y - mid_height + 10;
+
+        self_y_formatted = self_y_formatted.toString();
+        self_y_formatted = self_y_formatted.concat("px");
+
+        annotation.style.top = self_y_formatted;
+
+    };
+}
+
+annotation.style.transitionDuration = "0s";
+
+var curr = elms[0];
+
+var new_annotation = annotations_dict[curr.textContent];
+
+annot_paragraph.textContent = new_annotation;
+
+var annotation_position = annotation.getBoundingClientRect();
+
+var mid_height = annotation_position.height / 2;
+
+var self_position = curr.getBoundingClientRect();
+var self_y_formatted = self_position.y - mid_height + 10;
+
+self_y_formatted = self_y_formatted.toString();
+self_y_formatted = self_y_formatted.concat("px");
+
+annotation.style.top = self_y_formatted;
